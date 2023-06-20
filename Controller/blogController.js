0d37a1db86e275cefdcd78e256e351f490c4b01c
Pratham Neupane <prathamneupane5@gmail.com>
2023-06-20 15:12:30 +0545

    displayed blogs

--- Controller/blogController.js
+++ Controller/blogController.js
@@ -3,10 +3,32 @@ const Blog = db.blog;
 
 exports.index = async (req, res) => {
 
-    res.render("index");
+const blogs = await db.blog.findAll();
+    console.log(blogs);
+    res.render("index", {blogs});
 };
 
 exports.renderBlog = async(req,res) =>{
     
     res.render("createblog");
 };
+
+exports.createBlog = async(req,res) =>{
+    console.log(req.file)
+    const{title,description}=req.body
+
+    db.blog.create({
+        title: title,
+        description: description,
+        image: "http://localhost:4000/" + req.file.filename,
+    });
+
+    res.redirect("/index");
+};
+
+
+// exports.showBlogs= async(req,res)=>{
+//     const blog = await db.blog.findAll();
+//     console.log(blog);
+
+// };
--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const port = 4000;
 const db= require("./Model/index");
 const ejs= require("ejs");
 const blogController = require ("./Controller/blogController");
+const {storage, multer} = require("./Services/multerConfig");
+const upload = multer({storage:storage})
+const path = require("path")
 app.set("view engine","ejs");
 
 db.sequelize.sync({force:false});
 
 app.get("/", blogController.index);
 
-app.get("/createblog", blogController.renderBlog)
+app.get("/createblog", blogController.renderBlog);
+
+app.post("/createblog",  upload.single("image"), blogController.createBlog);
+
+app.get("/index", blogController.index);
+
+app.use(express.static(path.join(__dirname,"Uploads")));
 
 
 
